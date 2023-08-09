dc00568cd5f17fc7c728f4b9c58baede533a970e
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-08-09 10:11:45 -0400

    Done

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,17 @@
-const Buttons = () => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+import satData from "./satData";
+import './styling.css';
+const Buttons = (props) => {
+  const { filterByType, setSat, displaySats } = props;
+    return (
+    <div className ='flex-container'>
+      {displaySats.map((sat, id) => {
+       return (
+        <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+        </button>
+        );
+      })}
+      <button onClick={()=> setSat(satData)}>All Orbits</button>
       </div>
   );
 };
